843f2bd777954427472b5f9bbc12a95d2d938a1a
Jim Stokely <jimstokely2@gmail.com>
2015-08-08 13:34:07 -0500

    Fixed errors: 1st round

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ var A = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ];
 var gl_How_Many_Plays = 0;
 
 //console.log(A[0]);
+//console.log(A[0][0]);
+//console.log(A[0][1]);
+//console.log(A[0][2]);
 //console.log(A[1]);
 //console.log(A[2]);
 
@@ -14,7 +17,7 @@ function rowWin() {
   for (var x = 0; x < 3; x++) {
 
     // Check each row for a win
-    if (A[x][0] === A[x][1] === A[x][2]) {
+    if ((A[x][0] === A[x][1]) && (A[x][0] === A[x][2])) {
 
       // Return true to indicate a player has won the game.
       return true;
@@ -30,7 +33,7 @@ function colWin() {
   for (var x = 0; x < 3; x++) {
 
     // Check each column for a win
-    if (A[0][x] === A[1][x] === A[2][x]) {
+    if ((A[0][x] === A[1][x]) && (A[0][x] === A[2][x])) {
 
       // Return true to indicate a player has won the game
       return true;
@@ -45,8 +48,8 @@ function colWin() {
 function diagWin() {
 
   // Check each diagonal for a win
-  if ((A[0][0] === A[1][1] === A[2][2]) ||
-       A[0][2] === A[1][1] === A[2][0]) {
+  if (((A[0][0] === A[1][1]) && (A[0][0] === A[2][2])) ||
+      ((A[0][2] === A[1][1]) && (A[0][2] === A[2][0]))) {
 
     // Return true to indicate a player has won the game
     return true;
@@ -71,26 +74,76 @@ function checkForWin() {
 
 function checkBoard() {
 
-  // Increment the number of plays thus far
-  gl_How_Many_Plays++;
-
   // Did a player win?
   if (checkForWin()) {
 
     // A player has won the game
 
     // ---> code here to show a player has won
+console.log("player has won");
+
   }
   else if (gl_How_Many_Plays === 9) {
 
     // Since no player has won the game, are all of the boxes filled?
 
     // --> code here to show the game is a draw
+console.log("There is a draw");
+
   }
   else {
 
     // Keep playing
+//console.log("Keep playing");
+
   }
 
 }
+
+function processClick(x, y) {
+
+  //
+  if (isNaN(A[x][y])) {
+
+    // This sqaure already has an X or O.
+    // --> code here to show an error - do NOT overwrite the square
+
+//console.log("isNaN");
+//console.log("A[" + x + "][" + y + "] = " + A[x][y]);
+//console.log(A[0]);
+
+    return;
+  }
+
+  gl_How_Many_Plays++;
+
+  // Overwrite the square with an X or O
+  if (gl_How_Many_Plays % 2) {
+
+    A[x][y] = 'X';
+//console.log(A[0]);
+    // write an X to the square
+    var str = x + "," + y;
+    document.getElementById(str).innerHTML = 'X';
+
+  }
+  else {
+
+    A[x][y] = 'Z';
+//console.log(A[0]);
+    // write an O to the square
+    var str = x + "," + y;
+    document.getElementById(str).innerHTML = 'Z';
+  }
+
+  if (checkBoard()) {
+
+  }
+
+}
+
+//var box4 = document.getElementById("1,0");
+//box4.onclick = processClick(1,0);
+
+
 
